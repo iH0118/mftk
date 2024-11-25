8ce493bba741f1341ade65165390b83317301be1
iH0118 <66252845+iH0118@users.noreply.github.com>
2024-11-25 21:58:12 +0100

    aaaaaaaaaaaaaaa

--- ui0118.c
+++ ui0118.c
@@ -7,13 +7,13 @@ ui0118_window *ui0118_create_window(
 {
     ui0118_window *window = malloc(sizeof(ui0118_window));
 
-    unsigned int renderer_flags = 
+    unsigned int renderer_flags =
         SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC;
 
     window->window = SDL_CreateWindow(
-        title, 
-        SDL_WINDOWPOS_UNDEFINED, 
-        SDL_WINDOWPOS_UNDEFINED, 
+        title,
+        SDL_WINDOWPOS_UNDEFINED,
+        SDL_WINDOWPOS_UNDEFINED,
         (int) size_x * UI0118_UNIT + 1,
         (int) size_y * UI0118_UNIT + 1,
         0
@@ -27,4 +27,51 @@ ui0118_window *ui0118_create_window(
     return window;
 }
 
+void redraw_widget(ui0118_widget *widget, int priority)
+{
+    if (priority < widget->redraw_priority) return;
+
+    //TODO: actually write this function
+}
+
+void redraw_widget_subtree(ui0118_widget **current, int priority)
+{
+    ui0118_widget *subtree_start;
+
+    redraw_widget(*current, priority);
+
+    if ((*current)->type == UI0118_WIDGET_CONTAINER)
+    {
+        subtree_start = *current;
+
+        do redraw_widget_subtree(current, priority);
+        while (*current != subtree_start);
+    }
+
+    *current = (*current)->next;
+}
+
+void redraw_widget_tree(ui0118_widget *start, int priority)
+{
+    ui0118_widget *current = start;
+
+    do redraw_widget_subtree(&current, priority);
+    while (current != start);
+}
+
+int ui0118_redraw_window(ui0118_window *window, int priority)
+{
+    if (!window || !window->widget_top) return 1;
+
+
+
+    return 0;
+}
+
+void ui0118_insert_widget(ui0118_widget *widget, ui0118_widget *prev)
+{
+    ui0118_widget *temp = prev->next;
+    prev->next = widget;
+    widget->next = temp;
+}
 
--- ui0118.h
+++ ui0118.h
@@ -9,23 +9,23 @@ typedef struct ui0118_window ui0118_window;
 typedef struct ui0118_widget ui0118_widget;
 typedef union ui0118_widget_data ui0118_widget_data;
 typedef struct ui0118_widget_data_container ui0118_widget_data_container;
-typedef struct ui0118_widget_data_array ui0118_widget_data_array;
 typedef struct ui0118_widget_data_toggle ui0118_widget_data_toggle;
 typedef struct ui0118_widget_data_toggle_mom ui0118_widget_data_toggle_mom;
 typedef struct ui0118_widget_data_led ui0118_widget_data_led;
 typedef struct ui0118_widget_data_rotary ui0118_widget_data_rotary;
-typedef struct ui0118_widget_data_text_static ui0118_widget_data_text_static;
+typedef struct ui0118_widget_data_text ui0118_widget_data_text;
+//typedef struct ui0118_widget_data_text_input ui0118_widget_data_text_input;
 
 typedef enum ui0118_widget_type
 {
     UI0118_WIDGET_CONTAINER,
-    UI0118_WIDGET_ARRAY,
     UI0118_WIDGET_TOGGLE,
     UI0118_WIDGET_TOGGLE_MOM,
     UI0118_WIDGET_LED_RED,
     UI0118_WIDGET_LED_AMBER,
     UI0118_WIDGET_ROTARY,
-    UI0118_WIDGET_TEXT
+    UI0118_WIDGET_TEXT,
+    //UI0118_WIDGET_TEXT_INPUT
 } ui0118_widget_type;
 
 struct ui0118_widget_data_container
@@ -34,14 +34,6 @@ struct ui0118_widget_data_container
     ui0118_widget *child_bottom;
     unsigned int padding;
     SDL_Color color_bg;
-};
-
-struct ui0118_widget_data_array
-{
-    ui0118_widget **members;
-    unsigned int count;
-    int spacing_x;
-    int spacing_y;
 };
 
 struct ui0118_widget_data_toggle
@@ -71,7 +63,7 @@ struct ui0118_widget_data_rotary
     char state;
 };
 
-struct ui0118_widget_data_text_static
+struct ui0118_widget_data_text
 {
     char *text;
     unsigned int width;
@@ -81,12 +73,11 @@ struct ui0118_widget_data_text_static
 union ui0118_widget_data
 {
     ui0118_widget_data_container container;
-    ui0118_widget_data_array array;
     ui0118_widget_data_toggle toggle;
     ui0118_widget_data_toggle_mom toggle_mom;
     ui0118_widget_data_led led;
     ui0118_widget_data_rotary rotary;
-    ui0118_widget_data_text_static text;
+    ui0118_widget_data_text text;
 };
 
 struct ui0118_widget
@@ -97,6 +88,7 @@ struct ui0118_widget
     ui0118_widget_data data;
     int offset_x;
     int offset_y;
+    int redraw_priority;
 };
 
 struct ui0118_window
@@ -111,10 +103,14 @@ ui0118_window *ui0118_create_window(
     const char *title, unsigned int size_x, unsigned int size_y
 );
 
-ui0118_widget *ui0118_insert_widget(ui0118_widget *widget, ui0118_widget *prev);
+int ui0118_redraw_window(ui0118_window *window, int priority);
 
-ui0118_widget *ui0118_create_widget_container(ui0118_widget *prev);
+void ui0118_insert_widget(ui0118_widget *widget, ui0118_widget *prev);
 
-ui0118_widget *ui0118_create_widget_array(ui0118_widget *widget);
+ui0118_widget *ui0118_create_widget_container(
+    unsigned int padding, SDL_Color color_bg
+);
+
+ui0118_widget *ui0118_create_widget_array(ui0118_widget *child);
 
 #endif
