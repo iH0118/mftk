221ea1f1bb4236a2c768db86c72d83ec3bda6e3a
iH0118 <66252845+iH0118@users.noreply.github.com>
2024-11-28 08:46:07 +0100

    4dtcfr

--- ui0118.c
+++ ui0118.c
@@ -1,9 +1,11 @@
 #include "ui0118.h"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_pixels.h>
 #include <cjson/cJSON.h>
+#include <stdlib.h>
 
-ui0118_window *ui0118_create_window(
-    const char *title, unsigned int size_x, unsigned int size_y
+ui0118_window *ui0118_create_window(const char *title, unsigned int size_x,
+    unsigned int size_y, SDL_Color bg
 )
 {
     ui0118_window *window = malloc(sizeof(ui0118_window));
@@ -27,3 +29,129 @@ ui0118_window *ui0118_create_window(
     return window;
 }
 
+static ui0118_widget_type get_widget_type(const char *restrict type)
+{
+    if (!strcmp(type, "UI0118_CONTAINER")) return UI0118_CONTAINER;
+    if (!strcmp(type, "UI0118_TOGGLE")) return UI0118_TOGGLE;
+    if (!strcmp(type, "UI0118_TOGGLE_MOM")) return UI0118_TOGGLE_MOM;
+    if (!strcmp(type, "UI0118_LED_RED")) return UI0118_LED_RED;
+    if (!strcmp(type, "UI0118_LED_AMBER")) return UI0118_LED_AMBER;
+    if (!strcmp(type, "UI0118_ROTARY")) return UI0118_ROTARY;
+    if (!strcmp(type, "UI0118_TEXT")) return UI0118_TEXT;
+    if (!strcmp(type, "UI0118_TEXT_COUNT")) return UI0118_TEXT_COUNT;
+    return -1;
+}
+
+static SDL_Color get_json_color(cJSON *color_json)
+{
+    SDL_Color color;
+    color.r = cJSON_GetArrayItem(color_json, 0)->valueint;
+    color.g = cJSON_GetArrayItem(color_json, 1)->valueint;
+    color.b = cJSON_GetArrayItem(color_json, 2)->valueint;
+    color.a = cJSON_GetArrayItem(color_json, 3)->valueint;
+    return color;
+}
+
+ui0118_widget *create_widget_from_node(cJSON *node)
+{
+    ui0118_widget *widget = malloc(sizeof(ui0118_widget));
+
+    widget->type = get_widget_type(
+        cJSON_GetObjectItem(node, "type")->valuestring
+    );
+
+    widget->x = cJSON_GetObjectItem(node, "x")->valueint;
+    widget->y = cJSON_GetObjectItem(node, "y")->valueint;
+
+    cJSON *data = cJSON_GetObjectItem(node, "data");
+
+    switch (widget->type)
+    {
+        case UI0118_CONTAINER:
+            widget->data.container.size_x = 
+                cJSON_GetObjectItem(data, "size_x")->valueint;
+
+            widget->data.container.size_y =
+                cJSON_GetObjectItem(data, "size_y")->valueint;
+
+            SDL_Color color_bg;
+
+
+            widget->data.container.color = ; //TODO
+            break;
+
+        case UI0118_TOGGLE:
+            break;
+
+        case UI0118_TOGGLE_MOM:
+            break;
+
+        case UI0118_LED_RED:
+            break;
+
+        case UI0118_LED_AMBER:
+            break;
+
+        case UI0118_ROTARY:
+            break;
+
+        case UI0118_TEXT:
+            break;
+
+        case UI0118_TEXT_COUNT:
+            break;
+    }
+
+    return widget;
+}
+
+ui0118_window *ui0118_create_window_json(const char *restrict ui_json)
+{
+    cJSON *root = cJSON_Parse(ui_json);
+    cJSON *window_json = cJSON_GetObjectItem(root, "window");
+    cJSON *widgets_json = cJSON_GetObjectItem(root, "widgets");
+    cJSON *node;
+
+    SDL_Color window_bg;
+
+    window_bg.r = cJSON_GetArrayItem(
+        cJSON_GetObjectItem(window_json, "color_bg"), 0
+    )->valueint;
+
+    window_bg.g = cJSON_GetArrayItem(
+        cJSON_GetObjectItem(window_json, "color_bg"), 1
+    )->valueint;
+
+    window_bg.b = cJSON_GetArrayItem(
+        cJSON_GetObjectItem(window_json, "color_bg"), 2
+    )->valueint;
+
+    window_bg.a = cJSON_GetArrayItem(
+        cJSON_GetObjectItem(window_json, "color_bg"), 3
+    )->valueint;
+
+    ui0118_window *window = ui0118_create_window(
+        cJSON_GetObjectItem(window_json, "title")->valuestring,
+        cJSON_GetObjectItem(window_json, "size_x")->valueint,
+        cJSON_GetObjectItem(window_json, "size_y")->valueint,
+        window_bg
+    );
+
+    ui0118_widget **widget_next = &(window->widget_top);
+    ui0118_widget *widget_prev = NULL;
+
+    for (node = widgets_json->child; node; node = node->next)
+    {
+        ui0118_widget *widget = create_widget_from_node(node);
+
+        *widget_next = widget;
+        widget_next = &(widget->next);
+
+        widget->prev = widget_prev;
+        widget_prev = widget;
+    }
+
+    //TODO
+
+    return window;
+}
--- ui0118.h
+++ ui0118.h
@@ -2,6 +2,7 @@
 #define INCLUDE_UI0118_H
 
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_surface.h>
 
 #define UI0118_UNIT 11
 
@@ -14,6 +15,7 @@ typedef struct ui0118_widget_data_toggle_mom ui0118_widget_data_toggle_mom;
 typedef struct ui0118_widget_data_led ui0118_widget_data_led;
 typedef struct ui0118_widget_data_rotary ui0118_widget_data_rotary;
 typedef struct ui0118_widget_data_text ui0118_widget_data_text;
+typedef struct ui0118_widget_data_text_count ui0118_widget_data_text_count;
 //typedef struct ui0118_widget_data_text_input ui0118_widget_data_text_input;
 
 typedef enum ui0118_widget_type
@@ -25,6 +27,7 @@ typedef enum ui0118_widget_type
     UI0118_LED_AMBER,
     UI0118_ROTARY,
     UI0118_TEXT,
+    UI0118_TEXT_COUNT,
     //UI0118_WIDGET_TEXT_INPUT
 } ui0118_widget_type;
 
@@ -72,6 +75,13 @@ struct ui0118_widget_data_text
     char line;
 };
 
+struct ui0118_widget_data_text_count
+{
+    unsigned int start;
+    unsigned int step;
+    unsigned int end;
+};
+
 union ui0118_widget_data
 {
     ui0118_widget_data_container container;
@@ -80,6 +90,7 @@ union ui0118_widget_data
     ui0118_widget_data_led led;
     ui0118_widget_data_rotary rotary;
     ui0118_widget_data_text text;
+    ui0118_widget_data_text_count text_count;
 };
 
 struct ui0118_widget
@@ -90,7 +101,6 @@ struct ui0118_widget
     ui0118_widget_data data;
     int x;
     int y;
-    int layer;
 };
 
 struct ui0118_window
@@ -98,11 +108,13 @@ struct ui0118_window
     SDL_Window *window;
     SDL_Renderer *renderer;
     ui0118_widget *widget_top;
-    //SDL_Color color;
+    SDL_Color color_bg;
 };
  
-ui0118_window *ui0118_create_window(
-    const char *title, unsigned int size_x, unsigned int size_y
+ui0118_window *ui0118_create_window(const char *title, unsigned int size_x,
+    unsigned int size_y, SDL_Color bg
 );
+
+ui0118_window *ui0118_create_window_json(const char *ui_json);
 
 #endif
